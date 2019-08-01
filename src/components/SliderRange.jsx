780986c8b323d1e86447a444c0cb71fb4cf3ead6
Lendified <lendified@Lendifieds-MacBook-Pro.local>
2019-08-01 10:36:06 -0400

    creating slider component

--- src/components/SliderRange.jsx
+++ src/components/SliderRange.jsx
@@ -4,8 +4,8 @@ const SliderRange = props => {
     return (
         <div className="form-group">
             
-            <div className="form-group my-5" onMouseDown={props.action}>
-                <input type="range" min={props.min} max={props.max} defaultValue={props.defaultValue} step={props.step} className="slider" id={props.idRange} />
+            <div className="form-group my-5">
+                <input type="range" min={props.min} max={props.max} defaultValue={props.defaultValue} step={props.step} className="slider" id={props.idRange} onMouseDown={props.action} onPointerDown={props.action}/>
                 <div id="myMoney" className="mt-3 form-control amount text-center slider-display-amount">$&nbsp;1.500</div> 
                 <label className="ml-2 text-center">{props.title}</label>
             </div>
